e7c8c7e136d37f7a478302ae1e33db32f58b1f8b
Thomas H Jones II <github@xanthia.com>
2015-10-02 00:19:18 +0000

    Simplified prep function

--- buildem.sh
+++ buildem.sh
@@ -3,3 +3,15 @@
 # Script to build the downloaded SRPMs
 #
 #################################################################
+
+function PrepBuildDirs() {
+   # Ensure RPM build-tree exists
+   test -d ${HOME}/rpmbuild || mkdir -p \
+         ${HOME}/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS} > /dev/null 2>&1
+
+   # Ensure RPM macro-definition file exists
+   test -f ${HOME}/.rpmmacros || echo '%_topdir %(echo $HOME)/rpmbuild' \
+      > ${HOME}/.rpmmacros
+}
+
+PrepBuildDirs
