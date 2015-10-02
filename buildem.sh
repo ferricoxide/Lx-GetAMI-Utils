ff83c66fa803bd4309da868a5a68ed6b9446b829
Thomas H Jones II <github@xanthia.com>
2015-10-02 00:54:39 +0000

    Add missing rpm-dependency handler

--- buildem.sh
+++ buildem.sh
@@ -3,7 +3,13 @@
 # Script to build the downloaded SRPMs
 #
 #################################################################
+RPMDEPS=(
+         rpm-build
+         dos2unix
+        )
 
+
+# Set up RPM build environment
 function PrepBuildDirs() {
    # Ensure RPM build-tree exists
    test -d ${HOME}/rpmbuild || mkdir -p \
@@ -23,5 +29,47 @@ function ExtractSource() {
   done
 }
 
+# Move .spec files to ${HOME}/rpmbuild/SPECS
+function HomeSpecs() {
+  local SPECFILES="/tmp/srpm_extract/*.spec"
+  mv ${SPECFILES} ${HOME}/rpmbuild/SPECS
+}
+
+# Move source files to ${HOME}/rpmbuild/SOURCES
+function HomeSources() {
+   (
+      cd /tmp/srpm_extract
+      # Move the non-archived sources
+      mv $(awk '/^Source/{print $2}' ${HOME}/rpmbuild/SPECS/*.spec | sed '{
+         /tar.gz/d
+         /.tgz/d
+         /.zip/d
+      }') ${HOME}/rpmbuild/SOURCES
+      # Move the archived sources
+      mv * ${HOME}/rpmbuild/SOURCES
+   )
+}
+
+# Install any missing RPM dependencies
+function GetMissingRPMS() {
+   for DEPEND in ${RPMDEPS[@]}
+   do
+      if [[ $(rpm --quiet -q ${DEPEND})$? -ne 0 ]]
+      then
+         ADDRPMS="${ADDRPMS} ${DEPEND}"
+      fi
+   
+      if [[ "${ADDRPMS}" = "" ]]
+      then
+         echo "No missing RPMs detected"
+      else
+         yum install -q -y ${ADDRPMS}
+      fi
+   done
+)
+
+GetMissingRPMS
 PrepBuildDirs
 ExtractSource
+HomeSpecs
+HomeSources
